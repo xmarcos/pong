467258ef7d38295de07f06794e5512af30f3c91c
Marcos Sader <xmarcos@gmail.com>
2020-04-17 19:43:12 -0300

    Added the concept of a human and bot  to the Paddle class and functions to identify them

--- main.lua
+++ main.lua
@@ -13,7 +13,7 @@
 
     This version is built to more closely resemble the NES than
     the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
+    resolution, though in widescreen (16:9) so it looks nicer on
     modern systems.
 ]]
 
@@ -79,7 +79,7 @@ function love.load()
         ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
         ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
     }
-    
+
     -- initialize our virtual resolution, which will be rendered within our
     -- actual window no matter its dimensions
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -91,8 +91,8 @@ function love.load()
 
     -- initialize our player paddles; make them global so that they can be
     -- detected by other functions and modules
-    player1 = Paddle(10, 30, 5, 20)
-    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    player1 = Paddle(10, 30, 5, 20, Paddle:bot())
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20, Paddle:human())
 
     -- place a ball in the middle of the screen
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
@@ -304,7 +304,7 @@ function love.draw()
     push:start()
 
     love.graphics.clear(40/255, 45/255, 52/255, 1)
-    
+
     -- render different things depending on which part of the game we're in
     if gameState == 'start' then
         -- UI messages
@@ -314,7 +314,7 @@ function love.draw()
     elseif gameState == 'serve' then
         -- UI messages
         love.graphics.setFont(smallFont)
-        love.graphics.printf('Player ' .. tostring(servingPlayer) .. "'s serve!", 
+        love.graphics.printf('Player ' .. tostring(servingPlayer) .. "'s serve!",
             0, 10, VIRTUAL_WIDTH, 'center')
         love.graphics.printf('Press Enter to serve!', 0, 20, VIRTUAL_WIDTH, 'center')
     elseif gameState == 'play' then
@@ -330,7 +330,7 @@ function love.draw()
 
     -- show the score before ball is rendered so it can move over the text
     displayScore()
-    
+
     player1:render()
     player2:render()
     ball:render()
--- src/Paddle.lua
+++ src/Paddle.lua
@@ -11,6 +11,10 @@
     program to deflect the ball back toward the opponent.
 ]]
 
+-- workaround to get "Class Constants"
+local PADDLE_TYPE_BOT   = 'bot'
+local PADDLE_TYPE_HUMAN = 'human'
+
 Paddle = Class{}
 
 --[[
@@ -25,28 +29,52 @@ Paddle = Class{}
     instantiated at the time this function is called. Different objects can
     have their own x, y, width, and height values, thus serving as containers
     for data. In this sense, they're very similar to structs in C.
+
+    type is used to decide if the Paddle is controlled by a Human or the Machine.
 ]]
-function Paddle:init(x, y, width, height)
-    self.x = x
-    self.y = y
-    self.width = width
+function Paddle:init(x, y, width, height, type)
+    self.x      = x
+    self.y      = y
+    self.width  = width
     self.height = height
-    self.dy = 0
+    self.dy     = 0
+    -- ensure we only accep one of the two values (human, bot) or fallback to human
+    self.type   = (type == PADDLE_TYPE_HUMAN or type == PADDLE_TYPE_BOT) and type or PADDLE_TYPE_HUMAN
+end
+
+function Paddle:human()
+    return PADDLE_TYPE_HUMAN
+end
+
+function Paddle:bot()
+    return PADDLE_TYPE_BOT
+end
+
+function Paddle:isHuman()
+    return self.type == PADDLE_TYPE_HUMAN
+end
+
+function Paddle:isBot()
+    return self.type == PADDLE_TYPE_BOT
 end
 
 function Paddle:update(dt)
-    -- math.max here ensures that we're the greater of 0 or the player's
-    -- current calculated Y position when pressing up so that we don't
-    -- go into the negatives; the movement calculation is simply our
-    -- previously-defined paddle speed scaled by dt
-    if self.dy < 0 then
-        self.y = math.max(0, self.y + self.dy * dt)
-    -- similar to before, this time we use math.min to ensure we don't
-    -- go any farther than the bottom of the screen minus the paddle's
-    -- height (or else it will go partially below, since position is
-    -- based on its top left corner)
-    else
-        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    if self:isHuman() then
+        -- math.max here ensures that we're the greater of 0 or the player's
+        -- current calculated Y position when pressing up so that we don't
+        -- go into the negatives; the movement calculation is simply our
+        -- previously-defined paddle speed scaled by dt
+        if self.dy < 0 then
+            self.y = math.max(0, self.y + self.dy * dt)
+        -- similar to before, this time we use math.min to ensure we don't
+        -- go any farther than the bottom of the screen minus the paddle's
+        -- height (or else it will go partially below, since position is
+        -- based on its top left corner)
+        else
+            self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+        end
+    elseif self:isBot() then
+        self.dy = 0
     end
 end
 
